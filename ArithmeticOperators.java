966ba81cf9d85e3e2cca5f30c9f9ae8359ef296c
Arjunjoshi123 <34054384+Arjunjoshi123@users.noreply.github.com>
2019-02-07 16:38:16 +0530

    Create ArithmeticOperators.java

---
+++ ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package com.sonata;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int sum=0;
+		sum=a+b;
+		System.out.println("sum is"+sum);
+
+	}
+
+}
